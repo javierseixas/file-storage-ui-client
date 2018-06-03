0d7d4a38dd6e5028a9f690056f86165db24ad25c
Javier Seixas <dev@javierseixas.com>
2018-06-03 14:30:25 -0400

    Reformats code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from './Form'
@@ -20,20 +20,20 @@ class App extends Component {
         });
     };
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-          { this.state.messageFlashed &&
-            <FlashMessage message={this.state.message} />
-          }
-        <Form messageGetter={this.messageGetter} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                {this.state.messageFlashed &&
+                <FlashMessage message={this.state.message}/>
+                }
+                <Form messageGetter={this.messageGetter}/>
+            </div>
+        );
+    }
 }
 
 export default App;
