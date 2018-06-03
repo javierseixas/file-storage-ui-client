e140954e75c2487e4fdf24d711d93fb1f3c4e071
Javier Seixas <dev@javierseixas.com>
2018-06-03 14:05:35 -0400

    Reformats code

--- src/Form.js
+++ src/Form.js
@@ -17,7 +17,7 @@ export default class Form extends React.Component {
     }
 
     something = (some) => {
-        console.log("I do have state "+ this.state)
+        console.log("I do have state " + this.state)
     }
 
 
@@ -32,27 +32,26 @@ export default class Form extends React.Component {
         data.append("name", this.state.name);
         data.append("description", this.state.description);
 
-        let result = fetch("http://localhost:8080/files", {
-          method: "POST",
-          body: data
-        });
-
-        result.then(res => {
-            console.log(res);
-          if (res.ok) {
-              console.log("Perfect!")
-              responseMessage = "Perfect!";
-              return responseMessage;
-          } else if (res.status === 400) {
-              console.log("Ooops!");
-                responseMessage = "400!!!";
-              return responseMessage;
-          } else {
-              console.log("Whatever! " + res.status);
-              responseMessage = "Whatever!!!";
-              return responseMessage;
-          }
+        fetch("http://localhost:8080/files", {
+            method: "POST",
+            body: data
         })
+            .then(res => {
+                console.log(res);
+                if (res.ok) {
+                    console.log("Perfect!")
+                    responseMessage = "Perfect!";
+                    return responseMessage;
+                } else if (res.status === 400) {
+                    console.log("Ooops!");
+                    responseMessage = "400!!!";
+                    return responseMessage;
+                } else {
+                    console.log("Whatever! " + res.status);
+                    responseMessage = "Whatever!!!";
+                    return responseMessage;
+                }
+            })
             .then(response => {
                 this.setState({responseMessage: response});
                 console.log("what is here " + response);
@@ -60,8 +59,8 @@ export default class Form extends React.Component {
             .catch(error => {
                 console.log("Error submitting form!!" + e);
                 responseMessage = "Form error!!!";
-              return responseMessage;
-        });
+                return responseMessage;
+            });
 
         this.setState({
             name: '',
@@ -73,21 +72,25 @@ export default class Form extends React.Component {
     };
 
     render() {
-        return(
+        return (
             <form encType="multipart/form-data" method="POST">
                 <p>
                     <label>Name</label>
-                    <input name="name" placeholder='Name' value={this.state.name} onChange={e => this.change(e)} />
+                    <input name="name" placeholder='Name' value={this.state.name} onChange={e => this.change(e)}/>
                 </p>
                 <p>
                     <label>Description</label>
-                    <input name="description" placeholder='Description' value={this.state.description} onChange={e => this.change(e)} />
+                    <input name="description" placeholder='Description' value={this.state.description}
+                           onChange={e => this.change(e)}/>
                 </p>
                 <p>
                     <label>File</label>
-                    <input name="file" type="file" placeholder='Select file' value={this.state.file} onChange={e => this.change(e)} />
+                    <input name="file" type="file" placeholder='Select file' value={this.state.file}
+                           onChange={e => this.change(e)}/>
                 </p>
-                <p><button onClick={(e) => this.submit(e)}>Upload</button></p>
+                <p>
+                    <button onClick={(e) => this.submit(e)}>Upload</button>
+                </p>
 
             </form>
         )
